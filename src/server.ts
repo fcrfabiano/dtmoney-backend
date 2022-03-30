dad73a3f8d96dcd63a9bfccc946c4aa4affe2bf8
Fabiano César <fcfabiano.cesar@gmail.com>
2022-03-30 02:03:55 -0300

    Mudando porta

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,10 @@ import { routes } from './routes';
 import "./shared/container";
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('Server is running!'));
+app.listen(port, () => console.log('Server is running!'));
